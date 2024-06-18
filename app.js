aadbfd3213d9e953e4fef27d2e2c10c920e804f0
DineshGirbide <dinesh@incubyte.co>
2024-06-18 17:20:51 +0530

    multiple vendors

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ app.post('/validate-token', (req, res) => {
                     vendorDisplayName: 'SentinelOne',
                     logo_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyRIda6Q6lOqBXWB7jzz7gAsLDLcB9WJr_69Ci7tkgr0UkhydQYTIhmL77UX7nY1QfFZA&usqp=CAU',
                     docs_url: 'https://docs.leen.dev/integrations/tenable-credential',
-                    credentialsType: "OAuth2",
-                    leen_logo_url: "https://pbs.twimg.com/profile_images/1750375188657737728/8n7SuLiH_400x400.jpg",
                     dataSchema: {
                         type: 'object', properties: {
                             client_url: {
@@ -28,7 +26,7 @@ app.post('/validate-token', (req, res) => {
                             }, client_key: {
                                 type: 'string', password: true, description: 'SentinelOne Client Key', minLength: 1
                             }, secret_key: {
-                                type: 'string', password: true, description: 'SentinelOne Secret Key', minLength: 1
+                                type: 'string', description: 'SentinelOne Secret Key', minLength: 1
                             },
                         }, required: ['client_url', 'client_key', 'secret_key'],
                     },
@@ -41,6 +39,90 @@ app.post('/validate-token', (req, res) => {
                             type: 'Control', scope: '#/properties/secret_key',
                         },],
                     },
+                }, {
+                    vendor: 'NETSKOPE',
+                    vendorDisplayName: 'Netskope',
+                    logo_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdqEK-WXpvh35ZY_5hwMkoRkfRSK-6DWfPLMw9tlbgMVojT6M62GLktJ9MfA&s',
+                    docs_url: 'https://docs.leen.dev/integrations/tenable-credential',
+                    dataSchema: {
+                        type: 'object', properties: {
+                            client_key: {
+                                type: 'string', description: 'Netskope Client Key', minLength: 1
+                            }, secret_key: {
+                                type: 'string', description: 'Netskope Secret Key', minLength: 1
+                            },
+                        }, required: ['client_key', 'secret_key'],
+                    },
+                    uiSchema: {
+                        type: 'VerticalLayout', elements: [{
+                            type: 'Control', scope: '#/properties/client_key',
+                        }, {
+                            type: 'Control', scope: '#/properties/secret_key',
+                        },],
+                    },
+                }, {
+                    vendor: 'TENABLE',
+                    vendorDisplayName: 'Tenable',
+                    logo_url: 'https://pbs.twimg.com/profile_images/1410604377757216768/ocEKYniC_400x400.jpg',
+                    docs_url: 'https://docs.leen.dev/integrations/tenable-credential',
+                    dataSchema: {
+                        type: 'object', properties: {
+                            client_key: {
+                                type: 'string', description: 'Tenable Client Key', minLength: 1
+                            }, secret_key: {
+                                type: 'string', description: 'Tenable Secret Key', minLength: 1
+                            },
+                        }, required: ['client_key', 'secret_key'],
+                    },
+                    uiSchema: {
+                        type: 'VerticalLayout', elements: [{
+                            type: 'Control', scope: '#/properties/client_key',
+                        }, {
+                            type: 'Control', scope: '#/properties/secret_key',
+                        },],
+                    },
+                }, {
+                    vendor: 'CROWDSTRIKE',
+                    vendorDisplayName: 'CrowdStrike',
+                    logo_url: 'https://pbs.twimg.com/profile_images/1451022302578049024/6L-zG5oq_400x400.jpg',
+                    docs_url: 'https://docs.leen.dev/integrations/crowdstrike-credential',
+                    dataSchema: {
+                        type: 'object', properties: {
+                            client_id: {
+                                type: 'string', description: 'CrowdStrike Client ID', minLength: 1
+                            }, client_secret: {
+                                type: 'string', description: 'CrowdStrike Client Secret', minLength: 1
+                            }, base_url: {
+                                type: 'string', description: 'CrowdStrike Base URL', minLength: 1
+                            },
+                        }, required: ['client_id', 'client_secret', 'base_url'],
+                    },
+                    uiSchema: {
+                        type: 'VerticalLayout', elements: [{
+                            type: 'Control', scope: '#/properties/client_id',
+                        }, {
+                            type: 'Control', scope: '#/properties/client_secret',
+                        }, {
+                            type: 'Control', scope: '#/properties/base_url',
+                        },],
+                    },
+                }, {
+                    vendor: 'QUALYS',
+                    vendorDisplayName: 'Qualys',
+                    logo_url: 'https://s3-symbol-logo.tradingview.com/qualys--big.svg',
+                    docs_url: 'https://docs.leen.dev/integrations/crowdstrike-credential',
+                    dataSchema: {
+                        type: 'object', properties: {
+                            api_key: {
+                                type: 'string', description: 'Qualys API Key', minLength: 1
+                            }
+                        }, required: ['api_key'],
+                    },
+                    uiSchema: {
+                        type: 'VerticalLayout', elements: [{
+                            type: 'Control', scope: '#/properties/api_key',
+                        }],
+                    },
                 }],
             });
         }, 1000);
@@ -49,14 +131,9 @@ app.post('/validate-token', (req, res) => {
 
 const getAuthUrl = (authUrl) => {
     const redirectUri = 'https://api.incubyte.appmixer.cloud/auth/google/callback';
-    const scopes = [
-        'https://www.googleapis.com/auth/userinfo.email',
-        'https://www.googleapis.com/auth/userinfo.profile',
-    ];
+    const scopes = ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile',];
 
-    return `${authUrl}?client_id=777752744263-1n0fvs7vj0gd4hsnimhcvhmsh7p0f6m3.apps.googleusercontent.com&redirect_uri=${redirectUri}&scope=${scopes.join(
-        ' ',
-    )}&response_type=code&access_type=offline`;
+    return `${authUrl}?client_id=777752744263-1n0fvs7vj0gd4hsnimhcvhmsh7p0f6m3.apps.googleusercontent.com&redirect_uri=${redirectUri}&scope=${scopes.join(' ',)}&response_type=code&access_type=offline`;
 };
 
 app.post('/provisioning/organizations/:organizationId/connections', (req, res) => {
